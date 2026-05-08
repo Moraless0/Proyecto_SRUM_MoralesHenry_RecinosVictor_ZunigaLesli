d15c6415a8d14e6200d0ee97129b9ae3a1fef9a7
Moraless0 <moraless06@hotmail.com>
2026-05-07 19:19:44 -0600

    feat: :sparkles: Se agrega proveedores
    Se agregan las funciones de proveedores

---
+++ proveedores.py
@@ -0,0 +1,50 @@
+from persistence import load_json, save_json
+from validators import non_empty, email, phone, unique_code
+
+FILE = "proveedores.json"
+
+def cargar():
+    return load_json(FILE, [])
+
+def guardar(data):
+    save_json(FILE, data)
+
+def registrar():
+    data = cargar()
+    print("\n=== Registrar Proveedor ===")
+
+    while True:
+        codigo = non_empty("Código: ")
+        if unique_code(codigo, data, "codigo"):
+            break
+        print("⚠ Código repetido.")
+
+    empresa = non_empty("Empresa: ")
+    contacto = non_empty("Contacto: ")
+    direccion = non_empty("Dirección: ")
+    tel_cel = phone("Teléfono celular: ")
+    tel_fijo = phone("Teléfono fijo: ")
+    correo = email("Correo: ")
+
+    item = {
+        "codigo": codigo,
+        "empresa": empresa,
+        "contacto": contacto,
+        "direccion": direccion,
+        "tel_cel": tel_cel,
+        "tel_fijo": tel_fijo,
+        "correo": correo
+    }
+
+    data.append(item)
+    guardar(data)
+    print("✅ Registrado.")
+
+def listar():
+    data = cargar()
+    print("\n=== Proveedores ===")
+    if not data:
+        print("No hay registros.")
+        return
+    for p in data:
+        print(f"{p['codigo']} - {p['empresa']}")
