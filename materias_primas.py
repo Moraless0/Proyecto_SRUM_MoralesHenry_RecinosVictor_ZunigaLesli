efa7b8e3df14320122ff2efa3a19cc73c5b5765c
Moraless0 <moraless06@hotmail.com>
2026-05-07 21:29:55 -0600

    feat: :sparkles: Se agrega la funcion de agregar materia

---
+++ materias_primas.py
@@ -0,0 +1,39 @@
+from persistence import load_json, save_json
+from validators import non_empty, positive_int, non_negative_float, unique_code
+
+FILE = "materias_primas.json"
+
+def cargar():
+    return load_json(FILE, [])
+
+def guardar(data):
+    save_json(FILE, data)
+
+def registrar():
+    data = cargar()
+    print("\n=== Registrar Materia Prima ===")
+
+    while True:
+        codigo = non_empty("Código: ")
+        if unique_code(codigo, data, "codigo"):
+            break
+        print("⚠ Código repetido.")
+
+    nombre = non_empty("Nombre: ")
+    descripcion = non_empty("Descripción: ")
+    proveedor = non_empty("Proveedor: ")
+    stock = positive_int("Cantidad en stock: ")
+    precio = non_negative_float("Precio por unidad: ")
+
+    item = {
+        "codigo": codigo,
+        "nombre": nombre,
+        "descripcion": descripcion,
+        "proveedor": proveedor,
+        "stock": stock,
+        "precio": precio
+    }
+
+    data.append(item)
+    guardar(data)
+    print("✅ Registrado.")
